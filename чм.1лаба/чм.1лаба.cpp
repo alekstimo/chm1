83ee1ad5c1fd6f6332aeba02157dce0458a3daf9
Aleks Timonova <alekstim01@gmail.com>
2021-10-13 20:53:48 +0300

    ну тут конечно еще делать и делать

--- чм.1лаба/чм.1лаба.cpp
+++ чм.1лаба/чм.1лаба.cpp
@@ -11,6 +11,19 @@ void print(int k, int n, Rational* a, Rational* b, Rational* c, Rational* p, Rat
 void obnulSverhu(int i, int k, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f);
 void rabota(int k, int n, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f);
 bool proverka(int k, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f);
+int proverka2(int n, Rational* a, Rational* b);
+int proverka3(int n, Rational* a, Rational* c);
+bool proverka4(int k, int stop1);
+void change(int stop1, int stop2);
+void badnull(Rational* a, int k, int& nul1, int& nul2) {
+    Rational null(0, 1);
+    if (a[k - 1] == null) {
+        nul1 = k - 1;
+    }
+    if (a[k + 1] == null) {
+        nul2 = k + 1;
+    }
+}
 void readVector(ifstream& fin, int n, Rational* arr, bool flag) {
     if (!flag) n = n - 1;
     for (int i = 0; i < n; i++) {
@@ -90,34 +103,233 @@ void print(int k,int n, Rational* a, Rational* b, Rational* c, Rational* p, Rati
     
 }
 void rabota(int k,int n, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f) {
-    for (int i = 0; i < k; i++) {
-        edinica(i, k, n, a, b, c, p, q, f);
-        obnulSnizu(i, k,n, a, b, c, p, q, f);
-        //cout << proverka(k, a, b, c, p, q, f) << endl;
-        //print(k, n, a, b, c, p, q, f);
+    Rational null(0, 1);
+    int nul1=-1, nul2=-1;
+    badnull(a, k, nul1, nul2);
+    int stop1 = proverka2(n, a, b);
+    int stop2 = proverka3(n, a, c);
+    if (a[0] != null)  {                                        //не ноль на а[0][0]
+        if (a[n - 1] != null) {                                 //не ноль на a[n-1][n-1]
+            if (proverka4(k, stop1)||(stop1>k+1)) {               // ноль на главной и верхней побочной за испорченными или его нет
+                if (proverka4(k, stop2)||(stop2<k-1)) {           // ноль на главной и нижней побочной до испорченных или его нет
+                    for (int i = 0; i < k; i++) {               //если обоих нет случаев + если после сверху и до снизу + 
+                        edinica(i, k, n, a, b, c, p, q, f);     //+ если есть сверху после и нет снизу
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                    for (int i = n - 1; i > k; i--) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+                    edinica(k, k, n, a, b, c, p, q, f);
+                    for (int i = k; i < n - 1; i++) {
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                    for (int i = k; i > 0; i--) {
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+                }
+                else  if (stop1!=stop2){                                    
+                    if ((stop2 != -1)&&(stop1!=-1)) {                    // ноль на главной и нижней побочной после испорченных + ноль на главной и верхней побочной после 
+                        change(stop1, stop2);
+                        if (stop1 != stop2 + 1) {
+                            for (int i = 0; i < k; i++) {
+                                edinica(i, k, n, a, b, c, p, q, f);
+                                obnulSnizu(i, k, n, a, b, c, p, q, f);
+                            }
+                            for (int i = n - 1; i > stop1; i--) {
+                                edinica(i, k, n, a, b, c, p, q, f);
+                                obnulSverhu(i, k, a, b, c, p, q, f);
+
+                            }
+                            for (int i = stop1 - 1; i > stop2; i--) {
+                                edinica(i, k, n, a, b, c, p, q, f);
+                                obnulSverhu(i, k, a, b, c, p, q, f);
+
+                            }
+                            for (int i = stop2 - 1; i > k; i--) {
+                                edinica(i, k, n, a, b, c, p, q, f);
+                                obnulSverhu(i, k, a, b, c, p, q, f);
+
+                            }
+                            for (int i = k + 1; i < stop2; i++) {
+                                obnulSnizu(i, k, n, a, b, c, p, q, f);
+
+                            }
+                            for (int i = stop2 + 1; i < stop1; i++) {
+                                obnulSnizu(i, k, n, a, b, c, p, q, f);
+
+                            }
+                            for (int i = stop1 + 1; i < n - 1; i++) {
+                                obnulSnizu(i, k, n, a, b, c, p, q, f);
+
+                            }
+                            edinica(stop1, k, n, a, b, c, p, q, f);
+                            obnulSverhu(stop1, k, a, b, c, p, q, f);
+                            obnulSnizu(stop1, k, n, a, b, c, p, q, f);
+                            edinica(stop2, k, n, a, b, c, p, q, f);
+                            obnulSverhu(stop2, k, a, b, c, p, q, f);
+                            obnulSnizu(stop2, k, n, a, b, c, p, q, f);
+                            edinica(k, k, n, a, b, c, p, q, f);
+                            obnulSnizu(k, k, n, a, b, c, p, q, f);
+                            obnulSverhu(k, k, a, b, c, p, q, f);
+                            /*for (int i = k; i > 0; i--) {
+
+                                obnulSverhu(i, k, a, b, c, p, q, f);
+
+                            }*/
+                        }
+                    }
+                    else if (stop1 == -1) { //  если нет сверху и есть после снизу
+                       
+                        for (int i = 0; i < k; i++) {              
+                            edinica(i, k, n, a, b, c, p, q, f);     
+                            obnulSnizu(i, k, n, a, b, c, p, q, f);
+                        }
+                        for (int i = k-1; i > 0; i--) {
+                            obnulSverhu(i, k, a, b, c, p, q, f);
+                        }
+                        for (int i = k+1; i < n - 1; i++) {
+                            edinica(i, k, n, a, b, c, p, q, f);
+                            obnulSnizu(i, k, n, a, b, c, p, q, f);
+                        }
+                        edinica(n-1, k, n, a, b, c, p, q, f);
+                        for (int i = n - 1; i > k; i--) {
+                            obnulSverhu(i, k, a, b, c, p, q, f);
+                        }
+                    
+                        edinica(k, k, n, a, b, c, p, q, f);
+                        obnulSnizu(k, k, n, a, b, c, p, q, f);
+                        obnulSverhu(k, k, a, b, c, p, q, f);
+                        
+                    }
+                }
+            }
+            else if (proverka4(k, stop2) || (stop2 > k+1)) {                            //ноль на главной и верхней побочной до испорченных  + ноль на главной и нижней побочной за испорченными + что то еще я стерла нечаянно
+              
+                if (nul1!=-1) {
+                    if (nul1 != stop1 + 1) {
+                        for (int i = k - 2; i > 0; i--) {
+                            edinica(i, k, n, a, b, c, p, q, f);
+                            obnulSverhu(i, k, a, b, c, p, q, f);
+                          //  cout << proverka(k, a, b, c, p, q, f) << " ";
+                        }
+                        //cout << endl;
+                        edinica(0, k, n, a, b, c, p, q, f);
+                        for (int i = 0; i < k - 1; i++) {
+                            obnulSnizu(i, k, n, a, b, c, p, q, f);
+                          //  cout << proverka(k, a, b, c, p, q, f) << " ";
+                        }
+                        //cout << endl;
+                        edinica(k - 1, k, n, a, b, c, p, q, f);
+                        obnulSverhu(k - 1, k, a, b, c, p, q, f);
+                        obnulSnizu(k - 1, k, n, a, b, c, p, q, f);
+                        //cout << proverka(k, a, b, c, p, q, f) << " " << endl;
+                    }
+                }
+                else {
+                    for (int i = k - 1; i > 0; i--) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+                    edinica(0, k, n, a, b, c, p, q, f);
+                    for (int i = 0; i < k; i++) {
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                }
+
+                if (nul2 != -1) {
+                    if (nul2 != stop2 + 1) {
+                        for (int i = k + 2; i < n - 1; i++) {
+                            edinica(i, k, n, a, b, c, p, q, f);
+                            obnulSnizu(i, k, n, a, b, c, p, q, f);
+
+                        }
+                        edinica(n - 1, k, n, a, b, c, p, q, f);
+                        for (int i = n - 1; i > k+1; i--) {
+                            obnulSverhu(i, k, a, b, c, p, q, f);
+                        }
+                        edinica(k + 1, k, n, a, b, c, p, q, f);
+                        obnulSverhu(k + 1, k, a, b, c, p, q, f);
+                        obnulSnizu(k + 1, k, n, a, b, c, p, q, f);
+                    }
+                }
+                else {
+                    for (int i = k + 1; i < n - 1; i++) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                        //cout << proverka(k, a, b, c, p, q, f) << " ";
+                    }
+                    //cout << endl;
+                    edinica(n - 1, k, n, a, b, c, p, q, f);
+                    for (int i = n - 1; i > k; i--) {
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                      //  cout << proverka(k, a, b, c, p, q, f) << " ";
+                    }
+                   // cout << endl;
+                }
+                edinica(k, k, n, a, b, c, p, q, f);
+                obnulSverhu(k, k, a, b, c, p, q, f);
+                obnulSnizu(k, k, n, a, b, c, p, q, f);
+               // cout << proverka(k, a, b, c, p, q, f) << " " << endl;
+               
+            }
+            else if (stop1 != stop2) {              //ноль на главной и верхней побочной до испорченных + ноль на главной и нижней побочной до испорченных
+                change(stop1, stop2);
+                if (stop1 != stop2 + 1) {
+                    for (int i = n - 1; i > k; i--) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+
+                    for (int i = k + 1; i < n - 1; i++) {
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                    for (int i = 0; i < stop2; i++) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                    for (int i = stop2+1; i < stop1; i++) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                    for (int i = stop1+1; i < k - 1; i++) {
+                        edinica(i, k, n, a, b, c, p, q, f);
+                        obnulSnizu(i, k, n, a, b, c, p, q, f);
+                    }
+                    for (int i = k - 2; i > stop2; i--) {
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+                    for (int i = stop2 + 1; i > stop1; i--) {
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+                    for (int i = stop1 + 1; i > 0; i--) {
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+                    }
+                    edinica(stop1, k, n, a, b, c, p, q, f);
+                    obnulSverhu(stop1, k, a, b, c, p, q, f);
+                    obnulSnizu(stop1, k, n, a, b, c, p, q, f);
+                    edinica(stop2, k, n, a, b, c, p, q, f);
+                    obnulSverhu(stop2, k, a, b, c, p, q, f);
+                    obnulSnizu(stop2, k, n, a, b, c, p, q, f);
+                    edinica(k, k, n, a, b, c, p, q, f);
+                    obnulSnizu(k, k, n, a, b, c, p, q, f);
+                    obnulSverhu(k, k, a, b, c, p, q, f);
+                   /* for (int i = k; i > 0; i--) {
+
+                        obnulSverhu(i, k, a, b, c, p, q, f);
+
+                    }*/
+                }
+
+            }
+        }
     }
-    //print(k, n, a, b, c, p, q, f);
-    
-    for (int i = n - 1; i > k; i--) {
-        edinica(i, k, n, a, b, c, p, q, f);
-        obnulSverhu(i, k, a, b, c, p, q, f);
-        //cout << proverka(k, a, b, c, p, q, f) << endl;
-        //print(k, n, a, b, c, p, q, f);
-    }
-    //print(k, n, a, b, c, p, q, f);
-    edinica(k, k, n, a, b, c, p, q, f); 
-    for (int i = k; i < n-1; i++) {
-        
-        obnulSnizu(i, k, n, a, b, c, p, q, f);
-        // print(k, n, a, b, c, p, q, f);
-        //cout << proverka(k, a, b, c, p, q, f) << endl;
-    }
-    // print(k, n, a, b, c, p, q, f);
-    for (int i = k; i > 0; i--) {
-       
-        obnulSverhu(i, k, a, b, c, p, q, f);
-        //cout << proverka(k, a, b, c, p, q, f) << endl;
-        //print(k, n, a, b, c, p, q, f);
+}
+void change(int stop1, int stop2) {
+    if (stop1 < stop2) {
+        stop1 += stop2;
+        stop2 -= stop1;
+        stop1 -= stop2;
     }
 }
 bool proverka(int k, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f) {
@@ -125,11 +337,31 @@ bool proverka(int k, Rational* a, Rational* b, Rational* c, Rational* p, Rationa
         return 1;
     return 0;
 }
+int proverka2(int n, Rational* a, Rational* b) {
+    Rational null(0, 1);
+    for (int i = 1; i < n; i++) {
+        if ((a[i] == b[i-1]) && (b[i-1] == null))
+            return i;
+    }
+    return -1;
+}
+int proverka3(int n, Rational* a, Rational* c) {
+    Rational null(0, 1);
+    for (int i = 0; i < n-1; i++) {
+        if ((a[i] == c[i]) && (a[i] == null))
+            return i;
+    }
+    return -1;
+}
+bool proverka4(int k, int stop1) {
+    if ((stop1 == -1) || (stop1 == k))
+        return true;
+    return false;
+}
 void edinica(int i, int k,int n, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f) {
-    if (i <= k) {
+    
         Rational koef(a[i]);
         a[i] = a[i] / a[i];
-        b[i] = b[i] / koef;
         p[i] = p[i] / koef;
         f[i] = f[i] / koef;
         if (i == k) {
@@ -141,31 +373,30 @@ void edinica(int i, int k,int n, Rational* a, Rational* b, Rational* c, Rational
                 q[j] = q[j] / koef;
             }*/
         }
-    }
-    else {
-        Rational koef(a[i]);
-        a[i] = a[i] / a[i];
-        c[i-1] = c[i-1] / koef;
-        p[i] = p[i] / koef;
-        f[i] = f[i] / koef;
-    }
+        if (i > 0) {
+            c[i - 1] = c[i - 1] / koef;
+        }
+        if (i < n - 1) {
+            b[i] = b[i] / koef;
+        }
+    
 }
 void obnulSnizu(int i,int k,int n, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f) {
-    if (i < k) {
+    if (i != k) {
         Rational koef(c[i]);
         c[i] = c[i] - koef * a[i];
         a[i + 1] = a[i + 1] - koef * b[i];
         p[i + 1] = p[i + 1] - koef * p[i];
         f[i + 1] = f[i + 1] - koef * f[i];
         
-        if (i != k - 1) {
+        if ((i != k - 1)&&(i < k)) {
             koef = q[i];
             q[i] = q[i] - koef * a[i];
             q[i + 1] = q[i + 1] - koef * b[i];
             p[k] = a[k] = q[k] = q[k] - koef * p[i];
             f[k] = f[k] - koef * f[i];
         }
-        else {
+        else if (i < k ) {
             q[i+1] = p[i+1];
             q[i] = c[i];
         }
@@ -173,6 +404,7 @@ void obnulSnizu(int i,int k,int n, Rational* a, Rational* b, Rational* c, Ration
             b[i + 1] = p[i + 1];
             c[i + 1] = q[i + 1];
         }
+        
     }
     else if (i==k) {
         for (int j = k; j < n-1; j++) {
@@ -184,11 +416,12 @@ void obnulSnizu(int i,int k,int n, Rational* a, Rational* b, Rational* c, Ration
             f[j + 1] = f[j + 1] - koef * f[k];
         }
     }
-    else {
-        Rational koef(c[i]);
-        c[i] = c[i] - koef * a[i];
-        f[i + 1] = f[i + 1] - koef * f[i];
-    }
+    //else {
+    //    Rational koef(c[i]);
+    //    c[i] = c[i] - koef * a[i];
+    //    f[i + 1] = f[i + 1] - koef * f[i]; //дописать добавку когда обнуление снизу после испорченных с изменением столбца р
+    //    p[i + 1] = p[i + 1] - koef * p[i];
+    //}
 }
 
 void obnulSverhu(int i, int k, Rational* a, Rational* b, Rational* c, Rational* p, Rational* q, Rational* f) {
@@ -197,21 +430,24 @@ void obnulSverhu(int i, int k, Rational* a, Rational* b, Rational* c, Rational* 
     b[i - 1] = b[i - 1] - koef * a[i];
     f[i - 1] = f[i - 1] - koef * f[i];
     
-    if(i>k) {
+    if(i!=k) {
         a[i-1] = a[i-1] - koef * c[i-1];
         p[i-1] = p[i-1] - koef * p[i];
-        koef = q[i];
-        q[i] = q[i] - koef * a[i];
-        q[i-1] = q[i-1] - koef * c[i-1];
-        
-        if (i != k + 1) {
-            q[k] = a[k] = p[k] = p[k] - koef * p[i];
-            f[k] = f[k] - koef * f[i];
+        if (i > k) {
+            koef = q[i];
+            q[i] = q[i] - koef * a[i];
+            q[i - 1] = q[i - 1] - koef * c[i - 1];
+
+            if (i != k + 1) {
+                q[k] = a[k] = p[k] = p[k] - koef * p[i];
+                f[k] = f[k] - koef * f[i];
+            }
+            if (i == k + 2) {
+                c[i - 2] = p[i - 1];
+                b[i - 2] = q[i - 1];
+            }
         }
-        if (i == k + 2) {
-            c[i - 2] = p[i - 1];
-            b[i - 2] = q[i - 1];
-       }
+        
     }
     else if (i == k) {
         p[i - 1] = b[i - 1];
@@ -221,5 +457,5 @@ void obnulSverhu(int i, int k, Rational* a, Rational* b, Rational* c, Rational* 
             f[j] = f[j] - koef * f[k];
         }
     }
-    
+    //дописать на случай когда обнуление сверху до испорченных с изменением столбца p
 }
